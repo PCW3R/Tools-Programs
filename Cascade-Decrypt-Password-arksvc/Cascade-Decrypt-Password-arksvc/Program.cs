f24c4de56c90a0aba6e2e40afe7b59f385b66a1a
PCW3R <60108346+PCW3R@users.noreply.github.com>
2020-08-01 21:16:42 +0200

    Add program decrypt - HTB Cascade

---
+++ Cascade-Decrypt-Password-arksvc/Cascade-Decrypt-Password-arksvc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cascade_Decrypt_Password_arksvc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Crypto.DecryptString("BQO5l5Kj9MdErXx6Q6AGOw==", "c4scadek3y654321"));
+        }
+    }
+}
---
+++ Cascade-Decrypt-Password-arksvc/Cascade-Decrypt-Password-arksvc/Utils.cs
@@ -0,0 +1,53 @@
+﻿// CascCrypto.Crypto
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Crypto
+{
+	public const string DefaultIV = "1tdyjCbY1Ix49842";
+
+	public const int Keysize = 128;
+
+	public static string EncryptString(string Plaintext, string Key)
+	{
+		byte[] bytes = Encoding.UTF8.GetBytes(Plaintext);
+		Aes aes = Aes.Create();
+		aes.BlockSize = 128;
+		aes.KeySize = 128;
+		aes.IV = Encoding.UTF8.GetBytes("1tdyjCbY1Ix49842");
+		aes.Key = Encoding.UTF8.GetBytes(Key);
+		aes.Mode = CipherMode.CBC;
+		using (MemoryStream memoryStream = new MemoryStream())
+		{
+			using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+			{
+				cryptoStream.Write(bytes, 0, bytes.Length);
+				cryptoStream.FlushFinalBlock();
+			}
+			return Convert.ToBase64String(memoryStream.ToArray());
+		}
+	}
+
+	public static string DecryptString(string EncryptedString, string Key)
+	{
+		//Discarded unreachable code: IL_009e
+		byte[] array = Convert.FromBase64String(EncryptedString);
+		Aes aes = Aes.Create();
+		aes.KeySize = 128;
+		aes.BlockSize = 128;
+		aes.IV = Encoding.UTF8.GetBytes("1tdyjCbY1Ix49842");
+		aes.Mode = CipherMode.CBC;
+		aes.Key = Encoding.UTF8.GetBytes(Key);
+		using (MemoryStream stream = new MemoryStream(array))
+		{
+			using (CryptoStream cryptoStream = new CryptoStream(stream, aes.CreateDecryptor(), CryptoStreamMode.Read))
+			{
+				byte[] array2 = new byte[checked(array.Length - 1 + 1)];
+				cryptoStream.Read(array2, 0, array2.Length);
+				return Encoding.UTF8.GetString(array2);
+			}
+		}
+	}
+}
